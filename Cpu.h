c491d864db10d869ebff49fa6b8e1e9744dc5349
jonfpersson <jonfpersson@gmail.com>
2023-07-24 22:32:11 +0200

    Add main Load game into buffer

---
+++ Cpu.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#ifndef CPU
+#define CPU
+
+typedef unsigned char BYTE;
+typedef char SIGNED_BYTE;
+typedef unsigned short WORD;
+typedef signed short SIGNED_WORD;
+
+class Cpu {
+    public:
+    private:
+        BYTE m_Cartridge[0x200000] ;
+
+    void CPUReset()
+    {
+        // load in the game
+        FILE *in;
+        in = fopen("game", "rb");
+        fread(&m_Cartridge[0], 0x200000, 1, in);
+        fclose(in);
+    }
+
+};
+
+#endif
